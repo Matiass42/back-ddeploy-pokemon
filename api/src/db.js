7ff078479b869149da05e65dff7e9aa48a2cdad4
matiass42 <matiassvelazquez@gmail.com>
2023-03-02 17:31:52 -0300

    listo

--- api/src/db.js
+++ api/src/db.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
+ //  postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
    {
       logging: false, // set to console.log to see the raw SQL queries
